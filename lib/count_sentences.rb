89295a322e15b65be42b7e4d30975a06cfe8cc10
Edgar Irizarry <eirizarry15@yahoo.com>
2018-10-12 19:09:28 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,30 @@ require 'pry'
 
 class String
 
-  def sentence?_self
-    
+  def sentence?
+    if self.end_with(".")
+      return true 
+    else
+      return false 
+    end 
   end
 
   def question?
-
+if self.end_with?("?")
+  return true 
+else 
+  return false 
   end
 
   def exclamation?
-
+if self.end_with?("!")
+  return true 
+else
+  return false 
   end
 
   def count_sentences
-
+   new_array  = self.split(/[.?!]/).reject {|string| string.empty?}
+   return new_array.length 
   end
 end
